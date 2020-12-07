3379c43b2d1f7808aae2c791472876f138bd52a9
Olesia <olesia.tk@gmail.com>
2020-12-07 03:19:54 +0200

    Add burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// function burgerMenu(selector) {
+//   let menu = $(selector);
+//   let button = menu.find('.nav-mob__btn');
+//   let links = menu.find('.nav-mob__link');
+//   let overlay = menu.find('.nav-mob__overlay');
+
+//   button.on('click', (e) => {
+//     e.preventDefault();
+//     toggleMenu();
+//   });
+
+//   links.on('click', () => toggleMenu());
+//   overlay.on('click', () => toggleMenu());
+
+//   function toggleMenu() {
+//     menu.toggleClass('nav-mob--active');
+
+//     if (menu.hasClass('nav-mob--active')) {
+//       $('body').css('overflow', 'hidden');
+//     } else {
+//       $('body').css('overflow', 'visible');
+//     }
+//   }
+// }
+// burgerMenu('.nav-mob');
+
+// const menu = document.querySelector('.nav-mob');
+// const toggler = document.querySelector('.nav-mob__btn');
+// toggler.onclick = function toggleSidebar() {
+//   menu.classList.toggle('nav-mob--active');
+//   toggler.classList.toggle('nav-mob__btn--active');
+// }
